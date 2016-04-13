ed7018004dcfb3a6ab65e57c77d5d17fc4be4c19
Gorka <gorka@aquigorka.com>
2016-04-13 19:17:18 -0300

    Adds comments

--- plugins/logger/handler.js
+++ plugins/logger/handler.js
@@ -1,9 +1,19 @@
+// this handler uses a common logger
 var logger = require('../../lib/logger');
 
 module.exports = (req, res, next) => {
+  // log all incoming requests
   var time = Date.now();
   logger.log('Request Logger: Incoming request: ' + req.url);
-  req.on('eror', () => { logger.error('Request Logger: Error: ' + req.url); });
-  req.on('end', () => { logger.log('Request Logger: Response time: ' + (Date.now() - time)); });
+
+  // log errors
+  req.on('eror', () => {
+    logger.error('Request Logger: Error: ' + req.url); });
+
+  // when the request ends ltes log the time it took to end the response
+  req.on('end', () => {
+    logger.log('Request Logger: Response time: ' + (Date.now() - time)); });
+
+  // carry on, this is just a middleware
   next();
 };
