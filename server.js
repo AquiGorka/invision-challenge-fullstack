0e5354551382fe9d281d093b2796a17df40cf025
Gorka <gorka@aquigorka.com>
2016-04-13 19:14:21 -0300

    Adds comments for server

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ var PORT = process.argv[2] || process.env.PORT || 8080,
 var arithmeticExpression = require('./plugins/arithmetic-expression'),
   logger = require('./plugins/logger');
 
+/*
+  Define middlewares and handlers to be used by this server
+  Routes can be set/namespaced here in order to use middlewares & handlers for all requests or for specific routes
+  Add as many middlewares/handlers as needed:
+    .use( ':path', pluginInstaller() )
+  Fire up server at the desired PORT
+*/
 connect()
   // middleware
   .use( '/', logger() )
   // handlers
-  .use( '/', arithmeticExpression() )
+  .use( '/arithmetic-expression', arithmeticExpression() )
   // listen
   .listen(PORT, () => { console.log('Server listening on port: ', PORT); })
