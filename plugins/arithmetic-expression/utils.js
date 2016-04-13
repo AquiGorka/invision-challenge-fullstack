a4d66fd0c10381955bbf6042dcb7d71bb472a281
Gorka <gorka@aquigorka.com>
2016-04-13 18:35:27 -0300

    Adds comments to utils code

--- plugins/arithmetic-expression/utils.js
+++ plugins/arithmetic-expression/utils.js
@@ -1,3 +1,8 @@
+/**
+ * Parses an expression of the form positive integer, plus sign, positive integer, equal sign and returns an object with the two integers
+ * @param {string} str The expression
+ * @returns {object} An object with two integer values
+ */
 var parseExpression = str => {
   var data = str.replace('=', '').split('+');
   return {
@@ -6,6 +11,14 @@ var parseExpression = str => {
   };
 };
 
+/**
+ * Returns an arithmetic expression between two numbers.
+ * @param {int} v1 The first number.
+ * @param {int} v2 The second number.
+ * @param {string} [+] op The arithmetic operand to resolve.
+ * @returns {int} The sum of the two numbers if the operand is the plus sign.
+ * @returns {null} Returns null if the operand is not the plus sign.
+ */
 var evaluate = (v1, v2, op) => {
   var final = null;
   switch(op) {
@@ -16,9 +29,15 @@ var evaluate = (v1, v2, op) => {
   return final;
 };
 
+/**
+ * Adds two numbers together.
+ * @param {int} v1 The first number.
+ * @param {int} v2 The second number.
+ * @returns {int} The sum of the two numbers.
+ */
 var sum = (v1, v2) => {
-    return v1 + v2;
-  }
+  return v1 + v2;
+};
 
 module.exports = {
   parseExpression: parseExpression,
